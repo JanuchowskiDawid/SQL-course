6d4b015b3615483830029cef09bbe7e9f59cbd60
Dawid Januchowski <mmalpa97@wp.pl>
2022-06-08 20:04:36 +0200

    Creating tables and insterting values

--- sql1.sql
+++ sql1.sql
@@ -13,3 +13,18 @@ FROM customers
 INNER JOIN orders
 	ON orders.customerID = customers.customerID
 GROUP BY customers.customerID;
+
+CREATE TABLE people
+  (
+    first_name VARCHAR(20),
+    last_name VARCHAR(20),
+    age INT
+  );
+INSERT INTO people(first_name, last_name, age)
+VALUES ('Tina', 'Belcher', 13);
+INSERT INTO people(age, last_name, first_name)
+VALUES (42, 'Belcher', 'Bob');
+INSERT INTO people(first_name, last_name, age)
+VALUES('Linda', 'Belcher', 45)
+  ,('Phillip', 'Frond', 38)
+  ,('Calvin', 'Fischoeder', 70);
