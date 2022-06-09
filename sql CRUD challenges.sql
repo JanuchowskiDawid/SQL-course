c24cc90bfda83866e61a2e8fabd2d3de278a858a
Dawid Januchowski <mmalpa97@wp.pl>
2022-06-09 19:22:55 +0200

    Section 6 challenge

--- sql CRUD challenges.sql
+++ sql CRUD challenges.sql
@@ -3,3 +3,22 @@ DELETE FROM cats WHERE age=4;
 DELETE FROM cats WHERE age = cat_id;
 
 DELETE FROM cats;
+
+SELECT article, color, shirt_size, last_worn FROM shirts WHERE shirt_size = 'M';
+
+UPDATE shirts SET shirt_size = 'L' WHERE article='polo shirt';
+
+UPDATE shirts SET last_worn = 0 WHERE last_worn = 15;
+
+UPDATE shirts SET shirt_size='XS', color = 'off white' WHERE color = 'white'; 
+
+DELETE FROM shirts WHERE last_worn=200;
+
+DELETE FROM shirts WHERE article= 'tank top';
+
+DELETE FROM shirts;
+
+DROP TABLE shirts;
+
+show tables;
+
