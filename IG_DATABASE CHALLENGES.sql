e5404b1ca8dc40e2bd4db013df0546960627d1b5
Dawid Januchowski <mmalpa97@wp.pl>
2022-06-20 21:36:02 +0200

    modified 2 files

--- IG_DATABASE CHALLENGES.sql
+++ IG_DATABASE CHALLENGES.sql
@@ -1,18 +1,18 @@
-#Find the 5 oldest users
+#Find the 5 oldest users	1
 use ig_clone;
 
 SELECT * FROM users
 ORDER BY created_at ASC
 LIMIT 5;
 
-#What day of the week do most users register on?
+#What day of the week do most users register on?	2
 
 SELECT DAYNAME(created_at) FROM users
 GROUP BY DAYNAME(created_at)
 ORDER BY COUNT(id) DESC
 LIMIT 1;
 
-#Find the users who have never posted a photo
+#Find the users who have never posted a photo	3
 
 SELECT username 
 FROM users
@@ -20,7 +20,7 @@ LEFT JOIN photos
 	ON users.id = photos.user_id
 WHERE photos.user_id IS NULL;
 
-#We're running a new contest to see who can get the most likes on a single photo.
+#We're running a new contest to see who can get the most likes on a single photo.	4
 
 SELECT username, COUNT(likes.photo_id)
 FROM users
@@ -29,14 +29,14 @@ JOIN likes
 GROUP BY likes.photo_id
 ORDER BY COUNT(likes.photo_id) DESC
 LIMIT 1;
-
-#How many times does the average user post?
+	
+#How many times does the average user post?	5
 
 SELECT COUNT(photos.id)/COUNT(users.id) AS 'POSTS PER USER'
 FROM users
 LEFT JOIN photos ON users.id = photos.user_id;
 
-#What are the top 5 most commonly used hashtags?
+#What are the top 5 most commonly used hashtags?	6
 
 SELECT tag_name, COUNT(photo_tags.tag_id) AS uses
 FROM tags
@@ -45,7 +45,7 @@ GROUP BY tags.id
 ORDER BY uses DESC
 LIMIT 5;
 
-#Find users who have liked every single photo on the site
+#Find users who have liked every single photo on the site	7
 
 SELECT username FROM users
 JOIN likes ON user.id = likes.user_id
--- ONE_TO_MANY.sql
+++ ONE_TO_MANY.sql
@@ -1,3 +1,81 @@
 SELECT * FROM orders WHERE customer_id = (SELECT id FROM customers WHERE last_name= 'George');
 
 SELECT * FROM customers, orders; --wyświetla wszystkie wiersze połączone z każdym wierszem, każda możliwa kombinacja.
+
+SELECT * FROM customers, orders WHERE customers.id = orders.customer_id;
+
+SELECT * FROM customers
+JOIN orders
+	ON customers.id = orders.customer_id;
+
+SELECT * FROM customers
+LEFT JOIN orders
+	ON customers.id = orders.customer_id;
+
+SELECT 
+    first_name, 
+    last_name,
+    IFNULL(SUM(amount), 0) AS total_spent
+FROM customers
+LEFT JOIN orders
+    ON customers.id = orders.customer_id
+GROUP BY customers.id
+ORDER BY total_spent;
+
+SELECT 
+    IFNULL(first_name,'MISSING') AS first, 
+    IFNULL(last_name,'USER') as last, 
+    order_date, 
+    amount, 
+    SUM(amount)
+FROM customers
+RIGHT JOIN orders
+    ON customers.id = orders.customer_id
+GROUP BY first_name, last_name;
+
+CREATE TABLE students(
+id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+first_name VARCHAR(100)
+);
+
+
+CREATE TABLE papers(
+title VARCHAR(100),
+grade INT,
+student_id INT NOT NULL,
+FOREIGN KEY (student_id) REFERENCES students(id)
+);
+
+SELECT first_name, title, grade FROM students
+JOIN papers
+	ON students.id = papers.student_id
+ORDER BY grade DESC;
+ 
+
+SELECT first_name, title, grade FROM students
+LEFT JOIN papers
+	ON students.id = papers.student_id;
+
+SELECT first_name, IFNULL(title, 'MISSING'), IFNULL(grade,'0') FROM students
+LEFT JOIN papers
+	ON students.id = papers.student_id;
+
+SELECT first_name, IFNULL(AVG(grade), '0') AS average FROM students
+LEFT JOIN papers
+	ON students.id = papers.student_id
+GROUP BY first_name
+ORDER BY average DESC;
+
+
+SELECT 
+	first_name, 
+	IFNULL(AVG(grade), '0') AS average, 
+	CASE
+	WHEN AVG(grade) >=75 THEN 'PASSING'
+	ELSE 'FAILING'
+	END AS 'passing_status'
+	FROM students
+LEFT JOIN papers
+	ON students.id = papers.student_id
+GROUP BY first_name
+ORDER BY average DESC;
