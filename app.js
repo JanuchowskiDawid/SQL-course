8cd03f702094864d73455ef69d4eb1bd0205cf3e
Dawid Januchowski <mmalpa97@wp.pl>
2022-07-05 22:54:45 +0200

    added register function

--- app.js
+++ app.js
@@ -1,20 +1,21 @@
 var express = require ('express');
 var app = express ();
 var mysql = require('mysql');
- 
+
+app.set("view engine", "ejs");
+
 var connection = mysql.createConnection({
   host     : 'localhost',
   user     : 'root',
   database : 'join_us'
 });
 
-
 app.get("/", function(req, res){
- var q = 'SELECT COUNT(*) as count FROM users';
+ var q = 'SELECT COUNT(*) AS count FROM users';
  connection.query(q, function (error, results) {
  if (error) throw error;
- var msg = "We have " + results[0].count + " users";
- res.send(msg);
+	var count = results[0].count;
+	 res.render("home", {data: count});
  });
 });
 
@@ -30,4 +31,13 @@ app.get("/random_num", function(req, res){
 
 app.listen(3000, function(){
 	console.log("Sever running on 3000!");
+});
+
+app.post('/register', function(req,res){
+ var person = {email: req.body.email};
+ connection.query('INSERT INTO users SET ?', person, function(err, result) {
+ console.log(err);
+ console.log(result);
+ res.redirect("/");
+ });
 });
